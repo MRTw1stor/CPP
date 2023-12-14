0d857cb2307e34eee20fa2907dcfffb255aab13b
Otakawa <123019975+MRTw1stor@users.noreply.github.com>
2023-12-14 09:31:46 +0300

    Add files via upload

---
+++ line_proger/vichislenie.cpp
@@ -0,0 +1,21 @@
+/* Составить программу для вычисления */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    
+    // Ввод данных
+    float a,b,y;
+    cout<<"\n Введите число a = ";
+    cin>>a;
+    cout<<"\n Введите число b = ";
+    cin>>b;
+    cout<<"\n Введите число y = ";
+    cin>>y;
+    
+    // Считаем и выводим результат
+    int result = (7 * y + 3 * sin(a) + sqrt(pow(b, 2) + 19)) / (7 * y + sqrt(pow(b, 2) + 19) + 2);
+    cout<<"Резутьтат = "<<result;
+}
